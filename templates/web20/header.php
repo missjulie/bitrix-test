93277d9a2cd326b5977b9ee50a5910a30ff10d40
missjulie <emelyan256@mail.ru>
2012-07-12 14:24:53 +0400

    new template

--- templates/web20/header.php
+++ templates/web20/header.php
@@ -1,4 +1,5 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?$APPLICATION->ShowPanel()?>
 <html>
 <head>
 <?$APPLICATION->ShowHead()?>
@@ -7,114 +8,126 @@
 
 <body>
 
-
-<?$APPLICATION->ShowPanel();?>
-
-<div id="container">
-
-<div id="header">
-	<div id="header_text">
-		<?$APPLICATION->IncludeFile(
+<div id="mainWrap">
+<div id="mainPanel">
+ <div id="menu">
+  <ul>
+   <li><a href="#"><span>Home</span></a></li>
+   <li><div class="blank"></div></li>
+   <li><a href="#"><span>About&nbsp;Us</span></a></li>
+   <li><div class="blank"></div></li>
+   <li><a href="#"><span>Support</span></a></li>
+   <li><div class="blank"></div></li>
+   <li><a href="#"><span>Forum</span></a></li>
+   <li><div class="blank"></div></li>
+   <li><a href="#"><span>Development</span></a></li>
+   <li><div class="blank"></div></li>
+   <li><a href="#"><span>Conact&nbsp;Us</span></a></li>
+  </ul>
+ </div>
+  <div id="logoWrap"><h1>
+<?$APPLICATION->IncludeFile(
 			$APPLICATION->GetTemplatePath("include_areas/company_name.php"),
 			Array(),
 			Array("MODE"=>"html")
-		);?>
-	</div>
-
-	<div id="company_logo"></div>
-
-	<div id="search">
-		&nbsp;Поиск на сайте
-		<?$APPLICATION->IncludeComponent("bitrix:search.form", "flat", Array(
-			"PAGE"	=>	"/search/"
-			)
-	);?>
-	</div>
-
-	<div id="login">
-		<?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
+		);?></h1></div>
+ <div id="loginPanel">
+  <h2>User Login</h2>
+  <input name="name" type="text" value="User Name" onfocus="if(this.value=='User Name')this.value=''" onblur="if(this.value=='')this.value='User Name'"/>
+   <div class="blank"></div>
+  <input name="name" type="text" value="Password" onfocus="if(this.value=='Password')this.value=''" onblur="if(this.value=='')this.value='Password'"/>
+  <p>Not yet a Member? <a href="#">Register Now</a></p>
+  <a href="#" class="login">Login</a>
+<?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
 			"REGISTER_URL"	=>	"/auth/",
 			"PROFILE_URL"	=>	"/personal/profile/"
 			)
 		);?>
-	</div>
+ </div>
 
-	<div id="menu">
-	<?$APPLICATION->IncludeComponent(
-		"bitrix:menu", 
-		"tabs", 
-		Array(
-			"ROOT_MENU_TYPE"	=>	"top",
-			"MAX_LEVEL"	=>	"1",
-			"USE_EXT"	=>	"N",
-			"MENU_CACHE_TYPE" => "A",
-			"MENU_CACHE_TIME" => "3600",
-			"MENU_CACHE_USE_GROUPS" => "N",
-			"MENU_CACHE_GET_VARS" => Array()
-		)
-	);?>
-	</div>
+ <div id="quots"><p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet consectetur adipisci velit..."</p></div>
+ <div id="leftPanel">
+  <div class="about">
+   <h2>Somelines About Us</h2>
+   <h3>ltrices inh endrerit ac tempor idtellus uis quam uisque</h3>
+   <p><span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla libero leo, suscipit acfaucibusnon.</span><br /> Etiam est. Etiam ac ipsum sed pede ultricies pretium. Nulla mi lacus, fringilla eu, pulvinar at, commodo eu, dolor. Nam porttitor viverra sapien. Pellentesque vitae augue et lorem laoreet .</p>
+   <div class="date">
+    <div class="left">Saturday, April 26, 2008&nbsp;&nbsp;|</div>
+    <div class="right"><a href="#">Read More</a></div>
+   </div>
+  </div>
+  <div class="services">
+   <h2>Our Spectrum of Services</h2>
+   <div class="pic"><a href="#">Cras turpis duis at sapien eget nulla vulputate. </a></div>
+   <p><span>Donec accumsan porta risus.</span> Vestibulum sollicitudinlibero ut semper commodo, purus enim sollicitudin nulla, dictum vehicula diam lacus at purus. In egestas. </p>
+   <h3>Services</h3>
+   <ul>
+    <li><a href="#">Nam congue quam non augue ivamus porttitor</a></li>
+    <li><a href="#" class="bottom">Jam aongue buam n augue famus</a></li>
+   </ul>
+   <p><span>Pellentesque dolor.</span> Nunc congue est in ante mattis ultrices. liquam erat volutpat. Nam congue quam non augue. </p>
+  </div>
+  <div class="testimonial">
+   <h2>Testimonial</h2>
+   <p> <span>Nullam vel justo. Mauris viverra mattis neque.</span> Phasellus feugiat pede ut mauris uis orcies tibulum eleifend. Donec est purusiaculis acauctorNullam vel justo. Mauris viverra mattis neque. Phasellus feugiat pede ut mauris. Duis orci.</p>
+   <h3><span>by</span> David Jackson</h3>
+    <p><span>Vel justo. Mauris viverra mattis neque  asellus feugiat pede ut mauris uis orcies tibulum</span> eleifend. Donec est purusiaculis acauctorNullam vel justo. Mauris viverra mattis neque. Phasellus feugiat pede ut mauris.</p>
+   <h3><span>by</span> Tom Sam</h3>
+  </div>
+ </div>
+ <div id="rightPanel">
+  <h2>Photo Stock</h2>
+  <div class="pic1"></div>
+  <a href="#" class="view">view large</a>
+   <div class="pic2"></div>
+  <a href="#" class="view">view large</a>
+   <div class="pic3"></div>
+  <a href="#" class="view">view large</a>
+  <div class="contacts">
+   <h2>Quick Contact</h2>
+   <p>Name</p><input name="name" type="text" value="- enter your name -" onfocus="if(this.value=='- enter your name -')this.value=''" onblur="if(this.value=='')this.value='- enter your name -'"/>
+   <p>Email</p><input name="name" type="text" value="- enter your email -" onfocus="if(this.value=='- enter your email -')this.value=''" onblur="if(this.value=='')this.value='- enter your email -'"/>
+   <div class="blank2"></div><a href="#">Need a Quote?</a><div class="blank"></div><a href="#">Submit</a>
+  </div>
+  <div class="project">
+   <h2>Projects Link</h2>
+   <ul>
+    <li><a href="#">Lorem ipsum dolor sit amet eros consequat </a></li>
+    <li><a href="#">Consectetuer adipiscing elit</a></li>
+    <li><a href="#">Etiam quis est ut diam viverra rhoncus</a></li>
+    <li><a href="#">Fusce eros consequat </a></li>
+    <li><a href="#">Cras eros massa blandit </a></li>
+    <li><a href="#" class="bottom">Aoreet utdiam viverra</a></li>
+   </ul>
+  </div>
+ </div>
+ <div id="footPanel">
+  <div class="nav">
+   <ul>
+    <li><a href="#">Home</a></li>
+    <li><div class="blank">|</div></li>
+    <li><a href="#">About Us</a></li>
+    <li><div class="blank">|</div></li>
+    <li><a href="#">Suppor</a></li>
+    <li><div class="blank">|</div></li>
+    <li><a href="#">Forum</a></li>
+    <li><div class="blank">|</div></li>
+    <li><a href="#">Development</a></li>
+    <li><div class="blank">|</div></li>
+    <li><a href="#">Conact Us</a></li>
+   </ul>
+  </div>
+  <div class="copyright">© Copyright Infirmation Goes Here. All Rights Reserved.</div>
+  <p class="designInfo">Design by <a href="http://www.templateworld.com/">TemplateWorld</a> and brought to you by <a href="http://www.smashingmagazine.com/">SmashingMagazine</a></p>
+  <div class="validation">
+    <ul>
+     <li><a href="http://validator.w3.org/check?uri=referer">xhtml</a></li>
+     <li><div class="blank"></div></li>
+     <li><a href="http://jigsaw.w3.org/css-validator/check/referer">css</a></li>
+    </ul>  
+  </div>
+ </div>
 </div>
-
-<table id="content" cellpadding="0" cellspacing="0">
-	<tr>
-		<td rowspan="4" width="9" class="table-border-color"><div style="width:9px"></div></td>
-		<td width="4"><img src="<?=SITE_TEMPLATE_PATH?>/images/left_top_corner.gif" width="4" height="4" border="0" alt="" /></td>
-		<td align="right"><img src="<?=SITE_TEMPLATE_PATH?>/images/right_top_corner.gif" width="7" height="5" border="0" alt="" /></td>
-		<td rowspan="4" width="7" class="table-border-color"><div style="width:7px"></div></td>
-	</tr>
-	<tr>
-		<td class="left-column"><?$APPLICATION->IncludeComponent("bitrix:menu", "left", Array(
-	"ROOT_MENU_TYPE"	=>	"left",
-	"MAX_LEVEL"	=>	"1",
-	"CHILD_MENU_TYPE"	=>	"left",
-	"USE_EXT"	=>	"Y",
-	"MENU_CACHE_TYPE" => "A",
-	"MENU_CACHE_TIME" => "3600",
-	"MENU_CACHE_USE_GROUPS" => "Y",
-	"MENU_CACHE_GET_VARS" => array(
-		0 => "SECTION_ID",
-		1 => "page",
-	),
-	)
-);?>
-
-	<!-- SOCIALNETWORK -->
-
-
-<?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "sect", 
-					"AREA_FILE_SUFFIX" => "inc", 
-					"AREA_FILE_RECURSIVE" => "N", 
-					"EDIT_MODE" => "html", 
-					"EDIT_TEMPLATE" => "sect_inc.php" 
-				)
-			);?><?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page", 
-					"AREA_FILE_SUFFIX" => "inc", 
-					"AREA_FILE_RECURSIVE" => "N", 
-					"EDIT_MODE" => "html", 
-					"EDIT_TEMPLATE" => "page_inc.php" 
-					)
-			);?>
-			</td>
-		<td class="main-column">
-
-			<div id="printer"><noindex><a href="<?=htmlspecialchars($APPLICATION->GetCurUri("print=Y"));?>" title="Версия для печати" rel="nofollow">версия<br />для печати</a></noindex></div>
-
-			<div id="navigation"><?$APPLICATION->IncludeComponent(
-				"bitrix:breadcrumb",
-				".default",
-				Array(
-					"START_FROM" => "0", 
-					"PATH" => "", 
-					"SITE_ID" => "" 
-				)
-			);?></div>
-			<h1 id="pagetitle"><?$APPLICATION->ShowTitle(false)?></h1>
+</div>
+</body>
+</html>
